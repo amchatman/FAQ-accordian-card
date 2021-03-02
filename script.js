ecd91529c37187c49b44b44dc960b1323990b6c1
Ashley <ashchat2008@gmail.com>
2021-03-02 18:18:46 -0500

    javascript
    Added Javascript code and files

---
+++ script.js
@@ -0,0 +1,10 @@
+function openAnwser(){
+  document.getElementById('anwser1').style.display = "block";
+  document.getElementById('anwser2').style.display = "none";
+  document.getElementById('anwser3').style.display = "none";
+  document.getElementById('anwser4').style.display = "none";
+  document.getElementById('anwser5').style.display = "none";
+}
+function closeAnwser(){
+  document.getElementById('anwser1').style.display = "none";
+}
