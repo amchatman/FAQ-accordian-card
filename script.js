631c56f9d5045ce4e9c4037399cf5aff7997bbe2
Ashley <ashchat2008@gmail.com>
2021-03-03 18:49:27 -0500

    onclick
    javascript changes

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 function openAnwser(){
   document.getElementById('anwser1').style.display = "block";
-  document.getElementById('anwser2').style.display = "none";
-  document.getElementById('anwser3').style.display = "none";
-  document.getElementById('anwser4').style.display = "none";
-  document.getElementById('anwser5').style.display = "none";
 }
+function openAnwser2(){
+  document.getElementById('anwser2').style.display = "block";
+}
+function openAnwser3(){
+  document.getElementById('anwser3').style.display = "block";
+}
+function openAnwser4(){
+  document.getElementById('anwser4').style.display = "block";
+}
+function openAnwser5(){
+  document.getElementById('anwser5').style.display = "block";
+}
+
+
+
 function closeAnwser(){
   document.getElementById('anwser1').style.display = "none";
 }
