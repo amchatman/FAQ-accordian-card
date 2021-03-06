92dbe482e6488f22543ae9af52c3b3509fed643f
Ashley <ashchat2008@gmail.com>
2021-03-06 17:36:35 -0500

    Mobile Changes
    Added Mobile Comp

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
 function openAnwser(){
   document.getElementById('anwser1').style.display = "block";
 }
-function openAnwser2(){
-  document.getElementById('anwser2').style.display = "block";
-}
-function openAnwser3(){
-  document.getElementById('anwser3').style.display = "block";
-}
-function openAnwser4(){
-  document.getElementById('anwser4').style.display = "block";
-}
-function openAnwser5(){
-  document.getElementById('anwser5').style.display = "block";
-}
-
-
-
-function closeAnwser(){
-  document.getElementById('anwser1').style.display = "none";
-}
