7a8c727c6a70ef33ac7a9f93ed17d0d6cd1ef29b
Ashley <ashchat2008@gmail.com>
2021-03-07 10:26:49 -0500

    Fix Errors
    Fix Html errors

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-function openAnwser(){
-  document.getElementById('anwser1').style.display = "block";
+function openAnwser(answer1 , hide){
+  document.getElementById('answer1').classList.toggle(hide);
+}
+function openAnwser2(answer2 , hide){
+  document.getElementById('answer2').classList.toggle(hide);
+}
+function openAnwser3(answer3 , hide){
+  document.getElementById('answer3').classList.toggle(hide);
+}
+function openAnwser4(answer4 , hide){
+  document.getElementById('answer4').classList.toggle(hide);
+}
+function openAnwser5(answer5 , hide){
+  document.getElementById('answer5').classList.toggle(hide);
 }
